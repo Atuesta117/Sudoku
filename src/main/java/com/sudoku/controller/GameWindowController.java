574d8258317982a08e71fc5f0b7e7950e763df4e
Juan Jose <juanjoseatuestaflor@gmail.com>
2025-10-09 14:18:01 -0500

    the id of the textfield are set

--- src/main/java/com/sudoku/controller/GameWindowController.java
+++ src/main/java/com/sudoku/controller/GameWindowController.java
@@ -1,5 +1,116 @@
 package com.sudoku.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class GameWindowController {
+
+    @FXML
+    private TextField P1C1;
+
+    @FXML
+    private TextField P1C2;
+
+    @FXML
+    private TextField P1C3;
+
+    @FXML
+    private TextField P1C4;
+
+    @FXML
+    private TextField P1C5;
+
+    @FXML
+    private TextField P1C6;
+
+    @FXML
+    private TextField P2C1;
+
+    @FXML
+    private TextField P2C2;
+
+    @FXML
+    private TextField P2C3;
+
+    @FXML
+    private TextField P2C4;
+
+    @FXML
+    private TextField P2C5;
+
+    @FXML
+    private TextField P2C6;
+
+    @FXML
+    private TextField P3C1;
+
+    @FXML
+    private TextField P3C2;
+
+    @FXML
+    private TextField P3C3;
+
+    @FXML
+    private TextField P3C4;
+
+    @FXML
+    private TextField P3C5;
+
+    @FXML
+    private TextField P3C6;
+
+    @FXML
+    private TextField P4C1;
+
+    @FXML
+    private TextField P4C2;
+
+    @FXML
+    private TextField P4C3;
+
+    @FXML
+    private TextField P4C4;
+
+    @FXML
+    private TextField P4C5;
+
+    @FXML
+    private TextField P4C6;
+
+    @FXML
+    private TextField P5C1;
+
+    @FXML
+    private TextField P5C2;
+
+    @FXML
+    private TextField P5C3;
+
+    @FXML
+    private TextField P5C4;
+
+    @FXML
+    private TextField P5C5;
+
+    @FXML
+    private TextField P5C6;
+
+    @FXML
+    private TextField P6C1;
+
+    @FXML
+    private TextField P6C2;
+
+    @FXML
+    private TextField P6C3;
+
+    @FXML
+    private TextField P6C4;
+
+    @FXML
+    private TextField P6C5;
+
+    @FXML
+    private TextField P6C6;
 
 }
