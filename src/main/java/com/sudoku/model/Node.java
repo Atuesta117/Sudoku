188e2d56001a7e2dddf3154ea6090c649fef2e23
Atuesta117 <atuesta.juan@correounivalle.edu.co>
2025-10-15 13:12:29 -0500

    the window works better

--- src/main/java/com/sudoku/model/Node.java
+++ src/main/java/com/sudoku/model/Node.java
@@ -6,10 +6,12 @@ public class Node {
     private Float id;
     private String valor;
     private ArrayList<Node> children;
+    boolean isInitialValue;
     public Node(Float id) {
         this.id = id;
         this.children = new ArrayList<>();
         this.valor = " ";
+        this.isInitialValue = false;
     }
     public Float getId() {return id;}
     public void setId(Float id) {this.id = id;}
@@ -17,4 +19,6 @@ public class Node {
     public void setValor(String valor) {this.valor = valor;}
     public ArrayList<Node> getChildren() {return children;}
     public void addCHildren(Node nodo) {this.children.add(nodo);}
+    public boolean getIsInitialValue() {return isInitialValue;}
+    public void setIsInitialValue(boolean isInitialValue) {this.isInitialValue = isInitialValue;}
 }
